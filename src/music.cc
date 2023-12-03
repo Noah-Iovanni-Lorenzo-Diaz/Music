83ea4e8c7a313cf201bbefb9a9c6b77f62d267cb
Noah-Iovanni-Lorenzo-Diaz <alu0101633848@ull.edu.es>
2023-12-03 13:07:06 +0000

    Updated music.cc

--- src/music.cc
+++ src/music.cc
@@ -14,31 +14,35 @@ int MusicPlayer() {
     while (1==1) {
         
         // First input, PLAY or EXIT
-        std::cout << "Input \"PLAY\" to play the song, input \"EXIT\" to exit program\n";
-        std::string input1;
-        getline(std::cin, input1);
-        if (input1 == "EXIT") {
-            std::cout << "Exiting program...\n";
-            return -1;
-        } else if(input1 == "PLAY" ) {
-            std::cout << "Playing music\n";
-            PlaySound(TEXT(".music/InconvenienceStoreTheme.wav"), NULL, SND_ASYNC | SND_LOOP);
-        } else {
-            std::cout << " Wrong input, did you input PLAY or play?\n";
+        while (2==2) {
+            std::cout << "Input \"PLAY\" to play the song, input \"EXIT\" to exit program\n";
+            std::string input1;
+            getline(std::cin, input1);
+            if (input1 == "EXIT") {
+                std::cout << "Exiting program...\n";
+                return -1;
+            } else if(input1 == "PLAY" ) {
+                std::cout << "Playing music\n";
+                PlaySound(TEXT(".music/InconvenienceStoreTheme.wav"), NULL, SND_ASYNC | SND_LOOP);
+            } else {
+                std::cout << " Wrong input, did you input PLAY or play?\n";
+            }
         }
-        
+       
         // Second input, STOP or EXIT, dont input anything so the program keeps going
-        std::cout << "Input \"STOP\" to stop music, or \"EXIT\" to exit program\n";
-        std::string input2;
-        getline(std::cin, input2);
-        if (input2 == "STOP") {
-            PlaySound(0, 0, 0);
-            std::cout << "Stopped music\n";
-        } else if (input2 == "EXIT") {
-            std::cout << "Exiting program...\n";
-            return -1;
-        } else {
-            std::cout << " Wrong input, did you input Stop or stop?\n";
+        while(3==3) {
+            std::cout << "Input \"STOP\" to stop music, or \"EXIT\" to exit program\n";
+            std::string input2;
+            getline(std::cin, input2);
+            if (input2 == "STOP") {
+                PlaySound(0, 0, 0);
+                std::cout << "Stopped music\n";
+            } else if (input2 == "EXIT") {
+                std::cout << "Exiting program...\n";
+                return -1;
+            } else {
+                std::cout << " Wrong input, did you input Stop or stop?\n";
+            }
         }
     }
 }
